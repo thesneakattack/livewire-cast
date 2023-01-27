491297a61290418b014a4895e60ae774b8487a80
David F <davidfdz@gmail.com>
2023-01-27 22:14:26 +0000

    set overflow-y-auto to modal.blade.php and max-h to dialog.blade.php to hard-code a scrollable modal

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -28,7 +28,7 @@ switch ($maxWidth ?? '2xl') {
         break;
     case '2xl':
     default:
-        $maxWidth = 'sm:max-w-2xl';
+        $maxWidth = 'sm:max-w-4xl';
         break;
 }
 @endphp
@@ -71,7 +71,7 @@ switch ($maxWidth ?? '2xl') {
         <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
     </div>
 
-    <div x-show="show" class="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full {{ $maxWidth }}"
+    <div x-show="show" class="bg-white rounded-lg overflow-x-hidden overflow-y-auto shadow-xl transform transition-all sm:w-full {{ $maxWidth }}"
                     x-transition:enter="ease-out duration-300"
                     x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                     x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
--- resources/views/components/modal/dialog.blade.php
+++ resources/views/components/modal/dialog.blade.php
@@ -1,17 +1,19 @@
 @props(['id' => null, 'maxWidth' => null])
 
 <x-modal :id="$id" :maxWidth="$maxWidth" {{ $attributes }}>
+<div class="modal-content max-h-[600px]">
     <div class="px-6 py-4">
-        <div class="text-lg">
+        <div class="text-lg modal-header">
             {{ $title }}
         </div>
 
-        <div class="mt-4">
+        <div class="mt-4 modal-body">
             {{ $content }}
         </div>
     </div>
 
-    <div class="px-6 py-4 bg-gray-100 text-right">
+    <div class="px-6 py-4 text-right bg-gray-100 modal-footer">
         {{ $footer }}
     </div>
+</div>
 </x-modal>
