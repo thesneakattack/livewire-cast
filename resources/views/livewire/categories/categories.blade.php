7fceaf0320b32a8808e53d71bfc53943b53bf6d6
David F <davidfdz@gmail.com>
2023-01-29 10:28:54 +0000

    fixed Malformed foreach on categories.blade.php - was a whitespace issue

--- resources/views/livewire/categories/categories.blade.php
+++ resources/views/livewire/categories/categories.blade.php
@@ -147,12 +147,12 @@
 
                         <x-table.cell class="max-w-[150px]">
                             <ol>
-                                @foreach ( $category->lflbSubCategories->sortBy('position')->sortBy('title') as
-                                $sub_category)
+                                @foreach ( $category->lflbSubCategories->sortBy('position')->sortBy('title') as $sub_category)
                                 <li>
                                     <span class="font-medium text-cool-gray-900">{{ $sub_category->title }} </span>
                                 </li>
                                 @endforeach
+
                             </ol>
                         </x-table.cell>
 
