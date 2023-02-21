a25bff845754b2b3358095a35124ee1ec0e3f3d1
David F <davidfdz@gmail.com>
2023-02-21 22:02:57 +0000

    changed scrollability on modal

--- resources/views/components/modal/dialog.blade.php
+++ resources/views/components/modal/dialog.blade.php
@@ -1,13 +1,13 @@
 @props(['id' => null, 'maxWidth' => null])
 
 <x-modal :id="$id" :maxWidth="$maxWidth" {{ $attributes }}>
-    <div class="modal-content max-h-[900px]">
+    <div class="modal-content">
         <div class="px-6 py-4">
             <div class="text-lg modal-header">
                 {{ $title }}
             </div>
 
-            <div class="mt-4 modal-body">
+            <div class="mt-4 modal-body max-h-[450px] overflow-y-auto">
                 {{ $content }}
             </div>
         </div>
